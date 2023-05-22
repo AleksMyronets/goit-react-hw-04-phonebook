132104b866adb0ae7bcf3b2f1140bac04ef6ccd1
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-05-22 18:52:18 +0300

    Update Forma.jsx

--- src/components/Forma/Forma.jsx
+++ src/components/Forma/Forma.jsx
@@ -1,69 +1,73 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Input, FormBtn } from "./Forma.styled"
 
-export class Forma extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+export const Forma = ({ arr, onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  hendleSubmit = event => {
-    event.preventDefault();
-    const nameContacts = this.props.arr.map(el => el.name.toLowerCase());
-    if (nameContacts.includes(this.state.name.toLowerCase())) {
-      alert(`${this.state.name} is in your contacts`);
+  const hendleSubmit = e => {
+    e.preventDefault();
+    const nameContacts = arr.map(el => el.name.toLowerCase());
+    if (nameContacts.includes(name.toLowerCase())) {
+      alert(`${name} is in your contacts`);
     } else {
-      this.props.onSubmit(this.state.name, this.state.number);
-      this.reset();
+      onSubmit(name, number);
+      reset();
     }
   };
 
-  hendleNameTelChange = event => {
-    const { name, value } = event.currentTarget;
-    this.setState({ [name]: value });
+  const hendleNameTelChange = e => {
+    const { name, value } = e.currentTarget;
+    if (name === 'name ') setName(value);
+    if (number === 'namber') setNumber(value);
   };
 
-  reset = () => {
-    this.setState({ name: '', number: '' });
+  const reset = () => {
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    return (
-      <>
-        <form onSubmit={this.hendleSubmit}>
-          <label>
-            <Input
-              type="text"
-              name="name"
-              placeholder="Name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-              value={this.state.name}
-              onChange={this.hendleNameTelChange}
-            />
-          </label>
+  return (
+    <>
+      <form onSubmit={hendleSubmit}>
+        <label>
+          <Input
+            type="text"
+            name="name"
+            placeholder="Name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={name}
+            onChange={hendleNameTelChange}
+          />
+        </label>
 
-          <label>
-            <Input
-              type="tel"
-              name="number"
-              placeholder="Number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-              value={this.state.number}
-              onChange={this.hendleNameTelChange}
-            />
-          </label>
-          <FormBtn type="submit">Add contact</FormBtn>
-        </form>
-      </>
-    );
-  }
-}
+        <label>
+          <Input
+            type="tel"
+            name="number"
+            placeholder="Number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            value={number}
+            onChange={hendleNameTelChange}
+          />
+        </label>
+        <FormBtn type="submit">Add contact</FormBtn>
+      </form>
+    </>
+  );
+};
 
-Forma.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
+  Forma.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+    arr: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        number: PropTypes.string.isRequired,
+    })
+  )
 };
