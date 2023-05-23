3c8360a80419d41bcac62b4a9991223b7259d780
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-05-23 13:48:40 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -42,7 +42,7 @@ export const App = () => {
     window.localStorage.setItem('contacts', JSON.stringify('contacts'));
   }, [contacts]);
 
-  const visiblContacts = getVisibleContacts();
+  const VisibleContacts = getVisibleContacts();
 
   return (
     <ContainerForm>
@@ -54,7 +54,7 @@ export const App = () => {
         Contacts
       </h2>
       <Filter value={filter} onChange={changeFilter} />
-      <Contacts contacts={visiblContacts} onDeleteContacts={deleteContact} />
+      <Contacts contacts={VisibleContacts} onDeleteContacts={deleteContact} />
     </ContainerForm>
   );
 }
